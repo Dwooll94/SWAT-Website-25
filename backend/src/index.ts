a03bd13d5496b41474eb63c1cbf46daad580e107
Dillon Woollums <dwooll94@gmail.com>
2025-09-13 23:51:33 -0500

    trying to fix CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,15 +31,24 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again later.'
 });
 
+
 app.use(helmet());
+
+var whitelist = [process.env.FRONTEND_URL || 'http://localhost:3000', process.env.BACKEND_URL]
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true,
+  
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(limiter);
-
 // Serve uploaded files statically
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
